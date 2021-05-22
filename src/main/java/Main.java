b5bd7f269e3ef905309a79b1824c9e25d4313bdb
robertjankowski <robjanw11@wp.pl>
2021-05-22 16:13:22 +0200

    more results of q-voter model

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import simulation.Simulation;
 
 public class Main {
     public static void main(String[] args) {
-        Simulation simulation = new Simulation("config/test_beta_on_virtual.yaml");
-        simulation.runAll("test_beta_on_virtual");
+        Simulation simulation = new Simulation("config/test_q_voter_phase.yaml");
+        simulation.runAll("test_q_voter_phase");
     }
 }
