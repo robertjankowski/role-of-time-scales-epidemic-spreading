8cfba509c081534746dfc97c5ce2f777a458564f
robertjankowski <robjanw11@wp.pl>
2021-08-08 16:18:51 +0200

    running more experiments on link removal option

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mvn compile exec:java -Dexec.mainClass="pl.wf.Main"
--- src/main/java/pl/wf/Main.java
+++ src/main/java/pl/wf/Main.java
@@ -5,6 +5,6 @@ import pl.wf.simulation.Simulation;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Simulation simulation = new Simulation("config/results_remove_links_parameters_beta_range.yaml");
-        simulation.runAll("off", true);
+        simulation.runAll("off", false);
     }
 }
